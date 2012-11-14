a87afebf3bb8fcb8f571b04c6c4a05e3767519e2
Kyle Isom <coder@kyleisom.net>
2012-11-13 22:29:30 -0700

    Initial commit.

---
+++ config.go
@@ -0,0 +1,101 @@
+package config
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+)
+
+// ConfigMap is shorthand for the type used as a config struct.
+type ConfigMap map[string]map[string]string
+
+var (
+	configSection = regexp.MustCompile("^\\s*\\[\\s*(\\w+)\\s*\\]\\s*$")
+	configLine    = regexp.MustCompile("^\\s*(\\w+)\\s*=\\s*(.*)\\s*$")
+	commentLine   = regexp.MustCompile("^#.*$")
+	blankLine     = regexp.MustCompile("^\\s*$")
+)
+
+// ParseFile takes the filename as a string and returns a ConfigMap.
+func ParseFile(fileName string) (cfg ConfigMap, err error) {
+	cfg = make(ConfigMap, 0)
+	file, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	buf := bufio.NewReader(file)
+
+	var (
+		line           string
+		longLine       bool
+		currentSection string
+	)
+
+	fmt.Println("parsing")
+	for {
+		err = nil
+		lineBytes, isPrefix, err := buf.ReadLine()
+		if io.EOF == err {
+			break
+		} else if err != nil {
+			break
+		} else if isPrefix {
+			line += string(lineBytes)
+
+			longLine = true
+			continue
+		} else if longLine {
+			line += string(lineBytes)
+			longLine = false
+		} else {
+			line = string(lineBytes)
+		}
+		if commentLine.MatchString(line) {
+			continue
+		} else if blankLine.MatchString(line) {
+			continue
+		} else if configSection.MatchString(line) {
+			section := configSection.ReplaceAllString(line,
+				"$1")
+			fmt.Printf("section -> '%s'\n", section)
+			if section == "" {
+				err = fmt.Errorf("invalid structure in file")
+				break
+			} else if !cfg.SectionInConfig(section) {
+				cfg[section] = make(map[string]string, 0)
+			}
+			currentSection = section
+		} else if configLine.MatchString(line) {
+			key := configLine.ReplaceAllString(line, "$1")
+			val := configLine.ReplaceAllString(line, "$2")
+			if key == "" {
+				continue
+			}
+			cfg[currentSection][key] = val
+
+			fmt.Printf("'%s': '%s'\n", key, val)
+		}
+	}
+	return
+}
+
+// SectionInConfig determines whether a section is in the configuration.
+func (c *ConfigMap) SectionInConfig(section string) bool {
+	for s, _ := range *c {
+		if section == s {
+			return true
+		}
+	}
+	return false
+}
+
+// ListSections returns the list of sections in the config map.
+func (c *ConfigMap) ListSections() (sections []string) {
+	for section, _ := range *c {
+		sections = append(sections, section)
+	}
+	return
+}
---
+++ doc.go
@@ -0,0 +1,39 @@
+/*
+   Copyright (c) 2012 Kyle Isom <kyle@tyrfingr.is>
+
+   Permission to use, copy, modify, and distribute this software for any
+   purpose with or without fee is hereby granted, provided that the 
+   above copyright notice and this permission notice appear in all 
+   copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
+   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE 
+   AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+   DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
+   OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
+   TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+   PERFORMANCE OF THIS SOFTWARE.
+*/
+
+/*
+   package config provides parsing for simple configuration file.
+
+   A configuration is expected to be in the format
+
+        [ sectionName ]
+        key1 = some value
+        key2 = some other value
+        # we want to explain the importance and great forethought
+        # in this next value.
+        key3 = unintuitive value
+        [ anotherSection ]
+        key1 = a value
+        key2 = yet another value
+        ...
+
+   Blank lines are skipped, and lines beginning with # are considered
+   comments to be skipped. It is an error to have a section marker ('[]')
+   without a section name.
+*/
+package config
