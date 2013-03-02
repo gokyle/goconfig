49199a746ebd401a81ac81abadb081409770baf1
Kyle Isom <coder@kyleisom.net>
2013-03-02 00:23:22 -0700

    ConfigMaps may now be stored as well.
    This commit generally improves the usability of the ConfigMap. A
    ConfigMap may be created and built from scratch, and supporting
    methods have been added to hide the underlying implementation of
    a ConfigMap; in the future, the underlying implementation might
    change for performance reasons.

--- config.go
+++ config.go
@@ -22,7 +22,7 @@ var DefaultSection = "default"
 
 // ParseFile takes the filename as a string and returns a ConfigMap.
 func ParseFile(fileName string) (cfg ConfigMap, err error) {
-        var file *os.File
+	var file *os.File
 
 	cfg = make(ConfigMap, 0)
 	file, err = os.Open(fileName)
@@ -36,15 +36,15 @@ func ParseFile(fileName string) (cfg ConfigMap, err error) {
 		line           string
 		longLine       bool
 		currentSection string
-                lineBytes       []byte
-                isPrefix        bool
+		lineBytes      []byte
+		isPrefix       bool
 	)
 
 	for {
 		err = nil
 		lineBytes, isPrefix, err = buf.ReadLine()
 		if io.EOF == err {
-                        err = nil
+			err = nil
 			break
 		} else if err != nil {
 			break
@@ -75,12 +75,12 @@ func ParseFile(fileName string) (cfg ConfigMap, err error) {
 			}
 			currentSection = section
 		} else if configLine.MatchString(line) {
-                        if currentSection == "" {
-                                currentSection = DefaultSection
-                                if !cfg.SectionInConfig(currentSection) {
-                                        cfg[currentSection] = make(map[string]string, 0)
-                                }
-                        }
+			if currentSection == "" {
+				currentSection = DefaultSection
+				if !cfg.SectionInConfig(currentSection) {
+					cfg[currentSection] = make(map[string]string, 0)
+				}
+			}
 			key := configLine.ReplaceAllString(line, "$1")
 			val := configLine.ReplaceAllString(line, "$2")
 			if key == "" {
@@ -88,9 +88,9 @@ func ParseFile(fileName string) (cfg ConfigMap, err error) {
 			}
 			cfg[currentSection][key] = val
 		} else {
-                        err = fmt.Errorf("invalid config file")
-                        break
-                }
+			err = fmt.Errorf("invalid config file")
+			break
+		}
 	}
 	return
 }
@@ -110,5 +110,75 @@ func (c *ConfigMap) ListSections() (sections []string) {
 	for section, _ := range *c {
 		sections = append(sections, section)
 	}
+	return
+}
+
+// WriteFile writes out the configuration to a file.
+func (c *ConfigMap) WriteFile(filename string) (err error) {
+	file, err := os.Create(filename)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	for _, section := range c.ListSections() {
+		sName := fmt.Sprintf("[ %s ]\n", section)
+		_, err = file.Write([]byte(sName))
+		if err != nil {
+			return
+		}
+
+		for k, v := range (*c)[section] {
+			line := fmt.Sprintf("%s = %s\n", k, v)
+			_, err = file.Write([]byte(line))
+			if err != nil {
+				return
+			}
+		}
+		_, err = file.Write([]byte{0x0a})
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+// AddSection creates a new section in the config map.
+func (c *ConfigMap) AddSection(section string) {
+	if nil != (*c)[section] {
+		(*c)[section] = make(map[string]string, 0)
+	}
+}
+
+// AddKeyVal adds a key value pair to a config map.
+func (c *ConfigMap) AddKeyVal(section, key, val string) {
+	if "" == section {
+		section = DefaultSection
+	}
+
+	if nil == (*c)[section] {
+		c.AddSection(section)
+	}
+
+	(*c)[section][key] = val
+}
+
+// Retrieve the value from a key map.
+func (c *ConfigMap) GetValue(section, key string) (val string, present bool) {
+	if c == nil {
+		return
+	}
+
+	if section == "" {
+		section = DefaultSection
+	}
+
+	cm := *c
+	_, ok := cm[section]
+	if !ok {
+		return
+	}
+
+	val, present = cm[section][key]
 	return
 }
--- doc.go
+++ doc.go
@@ -2,17 +2,17 @@
    Copyright (c) 2012 Kyle Isom <kyle@tyrfingr.is>
 
    Permission to use, copy, modify, and distribute this software for any
-   purpose with or without fee is hereby granted, provided that the 
-   above copyright notice and this permission notice appear in all 
+   purpose with or without fee is hereby granted, provided that the
+   above copyright notice and this permission notice appear in all
    copies.
 
-   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL 
-   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
-   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE 
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+   WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
    AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
    DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA
-   OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER 
-   TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+   OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+   TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
    PERFORMANCE OF THIS SOFTWARE.
 */
 
@@ -38,6 +38,32 @@
    outside of a section, they are placed in the default section called
    "default".
 
-   Parsing a file can be done with the ParseFile function.
+   Parsing a file can be done with the ParseFile function. The sections
+   can be retrived with the ListSection method, and the ListKeys method
+   can be used to retrieve a list of keys. A value can be retrieved using
+   the GetValue method, which returns a string containing the value of
+   the key and a boolean that is true if the given key exists.
+
+   Creating a new configuration and writing it to a file is also
+   possible. For example, to create the previous example:
+
+        cmap := new(goconfig.ConfigMap)
+
+        cmap.AddSection("sectionName")
+        cmap.AddKeyVal("sectionName", "key1", "some value")
+        cmap.AddKeyVal("sectionName", "key2", "some other value")
+        cmap.AddKeyVal("sectionName", "key3", "unintuitive value")
+
+        cmap.AddSection("anotherSection")
+        cmap.AddKeyVal("anotherSection", "key1", "a value")
+        cmap.AddKeyVal("anotherSection", "key1", "yet another value")
+
+        err := cmap.WriteFile("example.conf")
+        if err != nil {
+                log.Fatal("couldn't write config file: ", err.Error())
+        }
+
+   Note that attempting to add sections or key / value pairs to a
+   nil ConfigMap will most likely result in panics.
 */
 package goconfig
